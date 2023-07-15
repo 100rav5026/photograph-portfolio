08ad25b298ceb6d11a15f8d231edea370478a63b
Sourav Mudaliar <souravmudaliar99@yahoo.com>
2023-07-15 13:36:16 +0530

    Web Pages using react router

--- photograph-portfolio/src/components/App.jsx
+++ photograph-portfolio/src/components/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import NavigationBar from "./TopNavigationBar";
-import Slideshow from './SlideShow';
-import HomeSectionTwo from './HomeSectionTwo';
+// import Slideshow from './SlideShow';
+// import HomeSectionTwo from './HomeSectionTwo';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from '../pages/Home';
+import AboutUs from '../pages/AboutUs';
+import ContactUs from '../pages/ContactUs';
 
 function App() {
   return (
     <div>
-      <NavigationBar/>
-      <Slideshow/>
-      <HomeSectionTwo/>
+      <BrowserRouter>
+        <NavigationBar/>
+          <Routes>
+            <Route exact path="/" Component={Home}/>
+            <Route path="about" Component={AboutUs}/>
+            <Route path="contact" Component={ContactUs}/>
+          </Routes>
+      </BrowserRouter>
     </div>)
 }
 
---
+++ photograph-portfolio/src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+        <Link to="/">Home</Link>    
+        <Link to="/about">About Us</Link>
+        <Link to="/contact">Contact Us</Link>
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
